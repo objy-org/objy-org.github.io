2ac1a447ccc4fec505b892cb4bbd2bbe63081bda
4ty711 <53232140+4ty711@users.noreply.github.com>
2020-03-23 20:50:54 +0100

    added vue and bredcrumbs

---
+++ index.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    currentPreview: 1,
+    previewItems: [{name: 'Objects', index: 1}, {name: 'Behaviours', index: 2}, {name: 'Simple API', index: 3}, {name: 'Extendable', index: 4}]
+  }
+})
