09364d7855df4614f7295be2008a108d72f6b376
4ty711 <53232140+4ty711@users.noreply.github.com>
2020-03-23 20:52:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var app = new Vue({
   el: '#app',
   data: {
     currentPreview: 1,
-    previewItems: [{name: 'Objects', index: 1}, {name: 'Behaviours', index: 2}, {name: 'Simple API', index: 3}, {name: 'Extendable', index: 4}]
+    previewItems: [{name: 'Just Objects', index: 1}, {name: 'W/ Behaviours', index: 2}, {name: 'Simple API', index: 3}, {name: 'Extendable', index: 4}]
   }
 })
