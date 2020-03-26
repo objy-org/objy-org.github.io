75e1bf1ac50ee73f97f6ae25a87748aa17dd8f90
4ty711 <53232140+4ty711@users.noreply.github.com>
2020-03-26 23:04:47 +0100

    changed lang to cli

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = new Vue({
   data: {
     currentPreview: 1,
     currentLang: 'JS',
-    langs: ['JS', 'OBJYlang'],
+    langs: ['JS', 'CLI'],
     previewItems: [{name: 'Everything is an object', index: 1}, {name: 'Objects have behaviours', index: 2}, {name: 'Simple CRUD API', index: 3}, {name: 'Extendable', index: 4}]
   }
 })
