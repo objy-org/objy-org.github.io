b234f69f1de4dbb4b4a1849b047a08e167b9dd01
4ty711 <53232140+4ty711@users.noreply.github.com>
2020-03-26 23:02:03 +0100

    added language selection

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = new Vue({
   el: '#app',
   data: {
     currentPreview: 1,
+    currentLang: 'JS',
+    langs: ['JS', 'OBJYlang'],
     previewItems: [{name: 'Everything is an object', index: 1}, {name: 'Objects have behaviours', index: 2}, {name: 'Simple CRUD API', index: 3}, {name: 'Extendable', index: 4}]
   }
 })
