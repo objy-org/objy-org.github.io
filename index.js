ccb261da3beb589c7aebed92f5a33e48b76231c7
Marco Booelling <marco.boelling@web.de>
2020-03-26 21:00:48 +0100

    fsa

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var app = new Vue({
   el: '#app',
   data: {
     currentPreview: 1,
-    previewItems: [{name: 'Just Objects', index: 1}, {name: 'W/ Behaviours', index: 2}, {name: 'Simple API', index: 3}, {name: 'Extendable', index: 4}]
+    previewItems: [{name: 'Everything is an object', index: 1}, {name: 'Objects have behaviours', index: 2}, {name: 'Simple CRUD API', index: 3}, {name: 'Extendable', index: 4}]
   }
 })
