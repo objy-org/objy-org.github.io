36b5fd4bdf4d50f629e72a95edf66eec5633fb8a
Marco Boelling <53232140+4ty711@users.noreply.github.com>
2020-08-11 12:19:51 +0200

    changed landing page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = new Vue({
   data: {
     currentPreview: 1,
     currentLang: 'JS',
-    langs: ['JS', 'CLI'],
+    langs: [/*'JS', 'CLI'*/],
     previewItems: [{name: 'Objects', index: 1}, {name: 'Behaviours', index: 2}, {name: 'CRUD API', index: 3}, {name: 'Extendable', index: 4}]
   }
 })
